cab7922d6b40c8b5fc992758354ef8c935a6a26b
sayantansnl <sayantansanyal72@gmail.com>
2026-06-10 18:58:55 +0530

    Added CLI access

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, World!")
+	args := os.Args
+	argsWithoutProg := args[1:]
+
+	if len(argsWithoutProg) < 1 {
+		fmt.Print("no website provided\n")
+		os.Exit(1)
+	}
+
+	if len(argsWithoutProg) > 1 {
+		fmt.Print("too many arguments provided\n")
+		os.Exit(1)
+	}
+
+	providedURL := argsWithoutProg[0]
+	fmt.Printf("starting crawl of %s", providedURL)
+	os.Exit(0)
 }
